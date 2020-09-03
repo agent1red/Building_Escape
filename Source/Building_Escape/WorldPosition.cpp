c9087210d69ea8fd266a255958c0046765c39346
agent1red <darklen14@gmail.com>
2020-09-03 16:46:59 -0400

    added assets for a door (testing)

---
+++ Source/Building_Escape/WorldPosition.cpp
@@ -0,0 +1,46 @@
+// Copyright Kevin Hudson 2020.
+
+
+#include "WorldPosition.h"
+#include "GameFramework/Actor.h"
+
+// Sets default values for this component's properties
+UWorldPosition::UWorldPosition()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UWorldPosition::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// UE_LOG(LogTemp, Warning, TEXT("This is a warning!"));
+	// UE_LOG(LogTemp, Error, TEXT("This is an error!"));
+	// UE_LOG(LogTemp, Display, TEXT("Just a log"));
+
+	FString ObjectName = GetOwner() -> GetName();
+
+	UE_LOG(LogTemp, Warning, TEXT("Objec name is: %s"), *GetOwner() -> GetName());
+
+	FString ObjectPosition = GetOwner() ->GetActorLocation().ToString();
+	FString ObjectRotation = GetOwner() ->GetActorTransform().GetRotation().ToString();
+	UE_LOG(LogTemp, Warning, TEXT("Object name is: %s. The object locations are: %s"), *ObjectName, *ObjectPosition);
+	UE_LOG(LogTemp, Warning, TEXT("Object's Rotation are: %s" ), *ObjectRotation);
+
+}
+
+
+// Called every frame
+void UWorldPosition::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
