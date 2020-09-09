6c03c3bee474c370b774817f8703dabee6df6bda
agent1red <darklen14@gmail.com>
2020-09-09 01:05:39 -0400

    Added DefaultPlayer and debugline

---
+++ Source/Building_Escape/Grabber.cpp
@@ -0,0 +1,73 @@
+// Copyright Kevin Hudson 2020.
+
+#include "DrawDebugHelpers.h"
+#include "Engine/World.h"
+#include "GameFramework/PlayerController.h"
+#include "Grabber.h"
+
+#define OUT
+
+
+// Sets default values for this component's properties
+UGrabber::UGrabber()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UGrabber::BeginPlay()
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("Grabber reporting for duty!"));
+	
+}
+
+
+// Called every frame
+void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+		// Get the players viewpoint 
+
+		FVector PlayerViewPointLocation;
+		FRotator PlayerViewPointRotation;
+
+	GetWorld() ->GetFirstPlayerController() ->GetPlayerViewPoint(
+		OUT PlayerViewPointLocation, 
+		OUT PlayerViewPointRotation
+	);
+
+		//logging out to test
+
+	// UE_LOG(LogTemp, Warning, TEXT("Location:%s  Rotation: %s"), 
+	// 	*PlayerViewPointLocation.ToString(), 
+	// 	*PlayerViewPointRotation.ToString()
+	// );
+
+	// Draw a line from player showing the reach
+
+	FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;
+
+	DrawDebugLine(
+		GetWorld(),
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FColor(0, 255, 0),
+		false,
+		0.f,
+		0,
+		5.f
+	);
+
+
+	// Ray-cast out to a certain distance (reach) private member variable 
+
+	// See what it hits (is that something we can pick up?) 
+}
+
